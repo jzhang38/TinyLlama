6035867b69442db7b34e58ee0898a6aca0a309a9
jzhang38 <a1286225768@gmail.com>
2023-12-02 21:27:25 +0800

    change log interval

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Loop through all yaml files with the prefix 'llama_19M' in the experiments directory
+# Loop through all yaml files with the prefix 'llama' in the experiments directory
 for file in experiments/node1/llama*.yaml; do
     # Run the python command with each file
     python pretrain/tinyllama.py --training_config "$file"
